0e3038df20da5c1b9591cbd9487334a9adebfc79
Diego Vicuña <diego704249@gmail.com>
2021-11-29 02:36:47 -0500

    Agregar archivos de proyecto.

---
+++ ECommerceAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ECommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public List<Category> GetCategories()
+        {
+            return new List<Category>
+            {
+                new Category
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Category 1"
+                },
+                new Category
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Category 1"
+                }
+            };
+        }
+    }
+}
